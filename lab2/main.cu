432be524ee7c97109eefaf20572d9722022c941e
Dasha <44597105+dkondakova@users.noreply.github.com>
2023-10-24 21:01:31 +0700

    Update main.cu

--- lab2/main.cu
+++ lab2/main.cu
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <vector>
+#include <chrono>
 #include <png.h>
 
-#define BLUR_RADIUS 7
+#define BLOCK_SIZE 16
+#define CHANNELS 3
+#define BLUR_RADIUS 8
+#define BLUR_FACTOR (1.0f / ((2 * BLUR_RADIUS + 1) * (2 * BLUR_RADIUS + 1)))
 
 bool loadImage(const char *filename, png_bytep **row_pointers, int &width, int &height) {
     FILE *file = nullptr;
@@ -28,7 +33,7 @@ bool loadImage(const char *filename, png_bytep **row_pointers, int &width, int &
 
         *row_pointers = (png_bytep *) malloc(sizeof(png_bytep) * height);
         for (int i = 0; i < height; ++i) {
-            (*row_pointers)[i] = (png_byte *) malloc(sizeof(png_byte) * width * 3);
+            (*row_pointers)[i] = (png_byte *) malloc(sizeof(png_byte) * width * CHANNELS);
         }
 
         png_read_image(png, (*row_pointers));
@@ -48,7 +53,7 @@ bool loadImage(const char *filename, png_bytep **row_pointers, int &width, int &
     }
 }
 
-bool saveImage(const char *filename, png_bytep **row_pointers, const int width, const int height) {
+bool saveImage(const char *filename, png_bytep **row_pointers, const int &width, const int &height) {
     FILE *file = nullptr;
     png_structp png = nullptr;
     png_infop info = nullptr;
@@ -87,188 +92,203 @@ bool saveImage(const char *filename, png_bytep **row_pointers, const int width, 
     }
 }
 
-__global__ void applyBlurGlobal(unsigned char *inputImage, 
-		unsigned char *outputImage, 
-		int width, int height, int blurRadius) {
+__global__ void applyBlurGlobal(unsigned char *inputImage, unsigned char *outputImage, int width, int height) {
     int col = blockIdx.x * blockDim.x + threadIdx.x;
     int row = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (col < width && row < height) {
-        int channels = 3;
-        int idx = (row * width + col) * channels;
-
-        float blurFactor = 1.0 / ((2 * blurRadius + 1) * (2 * blurRadius + 1));
+        int idx = (row * width + col) * CHANNELS;
 
         float3 pixelValue = make_float3(0.0f, 0.0f, 0.0f);
-        for (int dy = -blurRadius; dy <= blurRadius; ++dy) {
-            for (int dx = -blurRadius; dx <= blurRadius; ++dx) {
+        for (int dy = -BLUR_RADIUS; dy <= BLUR_RADIUS; ++dy) {
+            for (int dx = -BLUR_RADIUS; dx <= BLUR_RADIUS; ++dx) {
                 int neighborRow = row + dy;
                 int neighborCol = col + dx;
 
                 if (neighborRow >= 0 && neighborRow < height && neighborCol >= 0 && neighborCol < width) {
-                    int neighborIdx = (neighborRow * width + neighborCol) * channels;
+                    int neighborIdx = (neighborRow * width + neighborCol) * CHANNELS;
                     pixelValue.x += inputImage[neighborIdx];
                     pixelValue.y += inputImage[neighborIdx + 1];
                     pixelValue.z += inputImage[neighborIdx + 2];
-                }
-            }
-        }
-
-        outputImage[idx] = static_cast<unsigned char>(pixelValue.x * blurFactor);
-        outputImage[idx + 1] = static_cast<unsigned char>(pixelValue.y * blurFactor);
-        outputImage[idx + 2] = static_cast<unsigned char>(pixelValue.z * blurFactor);
-    }
-}
-
-__global__ void applyBlurShared(unsigned char *inputImage, 
-		unsigned char *outputImage, 
-		int width, int height, int blurRadius) {
-    __shared__ float3 sharedData[16][16];
-
-    int col = blockIdx.x * blockDim.x + threadIdx.x;
-    int row = blockIdx.y * blockDim.y + threadIdx.y;
-
-    if (col < width && row < height) {
-        int channels = 3;
-        int idx = (row * width + col) * channels;
-
-	sharedData[threadIdx.y][threadIdx.x] = make_float3(inputImage[idx],
-                                                  inputImage[idx + 1],
-                                                  inputImage[idx + 2]);
-    }
-
-    __syncthreads();
-
-    if (col < width && row < height) {
-        int channels = 3;
-        int idx = (row * width + col) * channels;
-
-        float blurFactor = 1.0 / ((2 * blurRadius + 1) * (2 * blurRadius + 1));
-
-        int sharedX = threadIdx.x;
-        int sharedY = threadIdx.y;
-
-        float3 pixelValue = make_float3(0.0f, 0.0f, 0.0f);
-        for (int dy = -blurRadius; dy <= blurRadius; ++dy) {
-            for (int dx = -blurRadius; dx <= blurRadius; ++dx) {
-                int sharedRow = sharedY + dy;
-                int sharedCol = sharedX + dx;
-
-		if (sharedRow >= 0 && sharedRow < 16 && sharedCol >= 0 && sharedCol < 16) {
-                    pixelValue.x += sharedData[sharedRow][sharedCol].x;
-                    pixelValue.y += sharedData[sharedRow][sharedCol].y;
-                    pixelValue.z += sharedData[sharedRow][sharedCol].z;
+                } else {
+		    pixelValue.x += 255.0f;
+                    pixelValue.y += 255.0f;
+                    pixelValue.z += 255.0f;
 		}
             }
         }
 
-        outputImage[idx] = static_cast<unsigned char>(pixelValue.x * blurFactor);
-        outputImage[idx + 1] = static_cast<unsigned char>(pixelValue.y * blurFactor);
-        outputImage[idx + 2] = static_cast<unsigned char>(pixelValue.z * blurFactor);
+        outputImage[idx] = static_cast<unsigned char>(pixelValue.x * BLUR_FACTOR);
+        outputImage[idx + 1] = static_cast<unsigned char>(pixelValue.y * BLUR_FACTOR);
+        outputImage[idx + 2] = static_cast<unsigned char>(pixelValue.z * BLUR_FACTOR);
+    }
+}
+
+__global__ void applyBlurShared(unsigned char *inputImage, unsigned char *outputImage, int width, int height) {
+    __shared__ float3 sharedData[BLOCK_SIZE + 2 * BLUR_RADIUS][BLOCK_SIZE + 2 * BLUR_RADIUS];
+
+    int col = blockIdx.x * blockDim.x + threadIdx.x - (2 * blockIdx.x + 1) * BLUR_RADIUS;
+    int row = blockIdx.y * blockDim.y + threadIdx.y - (2 * blockIdx.y + 1) * BLUR_RADIUS;
+
+    int idx = (row * width + col) * CHANNELS;
+    sharedData[threadIdx.y][threadIdx.x] = (col >= 0 && col < width && row >= 0 && row < height) ?
+	     make_float3(inputImage[idx], inputImage[idx + 1], inputImage[idx + 2]) :
+	     make_float3(255.0f, 255.0f, 255.0f);
+
+    __syncthreads();
+
+    if (col >= 0 && col < width && row >= 0 && row < height && 
+		    threadIdx.x >= BLUR_RADIUS && threadIdx.x < (blockDim.x - BLUR_RADIUS) &&
+		    threadIdx.y >= BLUR_RADIUS && threadIdx.y < (blockDim.y - BLUR_RADIUS)) {
+
+        float3 pixelValue = make_float3(0.0f, 0.0f, 0.0f);
+        for (int dy = -BLUR_RADIUS; dy <= BLUR_RADIUS; ++dy) {
+            for (int dx = -BLUR_RADIUS; dx <= BLUR_RADIUS; ++dx) {
+                int sharedRow = threadIdx.y + dy;
+                int sharedCol = threadIdx.x + dx;
+
+		if (sharedRow >= 0 && sharedRow < BLOCK_SIZE + 2 * BLUR_RADIUS && 
+				sharedCol >= 0 && sharedCol < BLOCK_SIZE + 2 * BLUR_RADIUS) {
+                    pixelValue.x += static_cast<float>(sharedData[sharedRow][sharedCol].x);
+                    pixelValue.y += static_cast<float>(sharedData[sharedRow][sharedCol].y);
+                    pixelValue.z += static_cast<float>(sharedData[sharedRow][sharedCol].z);
+		}
+            }
+        }
+
+        outputImage[idx] = static_cast<unsigned char>(pixelValue.x * BLUR_FACTOR);
+        outputImage[idx + 1] = static_cast<unsigned char>(pixelValue.y * BLUR_FACTOR);
+        outputImage[idx + 2] = static_cast<unsigned char>(pixelValue.z * BLUR_FACTOR);
     }
 }
 
 texture<unsigned char, 1, cudaReadModeElementType> texRef;
 
-__global__ void applyBlurTexture(unsigned char *outputImage, int width, int height, int blurRadius) {
+__global__ void applyBlurTexture(unsigned char *outputImage, int width, int height) {
     int col = blockIdx.x * blockDim.x + threadIdx.x;
     int row = blockIdx.y * blockDim.y + threadIdx.y;
 
     if (col < width && row < height) {
-        int channels = 3;
-        int idx = (row * width + col) * channels;
+        int idx = (row * width + col) * CHANNELS;
 
-        float blurFactor = 1.0 / ((2 * blurRadius + 1) * (2 * blurRadius + 1));
-
-        float3 pixelValue = make_float3(0.0f, 0.0f, 0.0f);
-        for (int dy = -blurRadius; dy <= blurRadius; ++dy) {
-            for (int dx = -blurRadius; dx <= blurRadius; ++dx) {
+	float3 pixelValue = make_float3(0.0f, 0.0f, 0.0f);
+        for (int dy = -BLUR_RADIUS; dy <= BLUR_RADIUS; ++dy) {
+            for (int dx = -BLUR_RADIUS; dx <= BLUR_RADIUS; ++dx) {
                 int neighborRow = row + dy;
                 int neighborCol = col + dx;
 
                 if (neighborRow >= 0 && neighborRow < height && neighborCol >= 0 && neighborCol < width) {
-                    int neighborIdx = (neighborRow * width + neighborCol) * channels;
+                    int neighborIdx = (neighborRow * width + neighborCol) * CHANNELS;
                     pixelValue.x += tex1Dfetch(texRef, neighborIdx);
                     pixelValue.y += tex1Dfetch(texRef, neighborIdx + 1);
                     pixelValue.z += tex1Dfetch(texRef, neighborIdx + 2);
+                } else {
+                    pixelValue.x += 255.0f;
+                    pixelValue.y += 255.0f;
+                    pixelValue.z += 255.0f;
                 }
             }
         }
 
-        outputImage[idx] = static_cast<unsigned char>(pixelValue.x * blurFactor);
-        outputImage[idx + 1] = static_cast<unsigned char>(pixelValue.y * blurFactor);
-        outputImage[idx + 2] = static_cast<unsigned char>(pixelValue.z * blurFactor);
+        outputImage[idx] = static_cast<unsigned char>(pixelValue.x * BLUR_FACTOR);
+        outputImage[idx + 1] = static_cast<unsigned char>(pixelValue.y * BLUR_FACTOR);
+        outputImage[idx + 2] = static_cast<unsigned char>(pixelValue.z * BLUR_FACTOR);
     }
 }
- 
-int main() {
-    int blurRadius = 7;
 
-    const char *inputFileName = "input.png";
-    const char *outputFileName = "output.png";
-    int width, height;
-
-    png_bytep *row_pointers;
-    if (!loadImage(inputFileName, &row_pointers, width, height)) return 1;
-
-    size_t imageSize = width * height * 3 * sizeof(unsigned char);
+void applyBlur(const unsigned char *inputImage, unsigned char *outputImage, size_t imageSize, int width, int height, const std::string& type) {
     unsigned char *d_inputImage, *d_outputImage;
     cudaMalloc((void **) &d_inputImage, imageSize);
     cudaMalloc((void **) &d_outputImage, imageSize);
-    
-    unsigned char *inputImage = new unsigned char[imageSize]; 
-    unsigned char *outputImage = new unsigned char[imageSize];
-
-    for (int i = 0; i < height; i++) {
-        memcpy(&inputImage[i * width * 3 * sizeof(unsigned char)],
-               row_pointers[i],
-               width * 3 * sizeof(unsigned char));
-    }
-
     cudaMemcpy(d_inputImage, inputImage, imageSize, cudaMemcpyHostToDevice);
 
-    dim3 GS(32, 32, 1);
-    dim3 BS(16, 16, 1);
-
-    //applyBlurShared<<<GS,BS>>>(d_inputImage, d_outputImage, width, height, blurRadius);
-
-    //applyBlurShared<<<GS,BS>>>(d_inputImage, d_outputImage, width, height, blurRadius);
-
-    /*cudaArray *cuArray;
-    cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(8, 0, 0, 0, cudaChannelFormatKindUnsigned);
-    cudaMallocArray(&cuArray, &channelDesc, imageSize);
-    cudaMemcpyToArray(cuArray, 0, 0, inputImage, imageSize, cudaMemcpyHostToDevice);
-    texRef.addressMode[0] = cudaAddressModeWrap;
-    texRef.addressMode[1] = cudaAddressModeWrap;
-    texRef.filterMode = cudaFilterModeLinear;
-    texRef.normalized = false;
-    cudaBindTextureToArray(texRef, cuArray, channelDesc);
-    applyBlurTexture<<<GS, BS>>>(d_outputImage, width, height, blurRadius);
-    cudaUnbindTexture(texRef);
-	*/
-    size_t offset = 0;
-    texRef.addressMode[0] = cudaAddressModeWrap;
-    texRef.addressMode[1] = cudaAddressModeWrap;
-    //texRef.filterMode = cudaFilterModeLinear;
-    //texRef.normalized = false;
-    cudaBindTexture(&offset, texRef, d_inputImage, imageSize);
-    applyBlurTexture<<<GS, BS>>>(d_outputImage, width, height, blurRadius);
-    cudaUnbindTexture(texRef);
+    if (type == "texture") {
+        //size_t offset = 0;
+        //texRef.addressMode[0] = cudaAddressModeWrap;
+        //texRef.addressMode[1] = cudaAddressModeWrap;
+        cudaBindTexture(0, texRef, d_inputImage, imageSize);
+    }
     
-    std::cout << static_cast<int>(inputImage[0]) << std::endl;
+    dim3 GS(width / BLOCK_SIZE, height / BLOCK_SIZE, 1);
+    dim3 BS(BLOCK_SIZE, BLOCK_SIZE, 1);
+    dim3 BS_s(BLOCK_SIZE + 2 * BLUR_RADIUS, BLOCK_SIZE + 2 * BLUR_RADIUS, 1);
+
+    //int deviceID = 0; // Идентификатор устройства
+    //cudaDeviceProp deviceProp;
+    //cudaGetDeviceProperties(&deviceProp, deviceID);
+    //int smCount = deviceProp.multiProcessorCount; // Количество SM на устройстве
+    //int maxBlocksPerSM; // Количство блоков, которые можно запустить на SM с учетом ограничений ресурсов
+    //cudaOccupancyMaxActiveBlocksPerMultiprocessor(&maxBlocksPerSM, applyBlurTexture, BS.x * BS.y, 0);
+    //int maxBlocks = maxBlocksPerSM * smCount; // Максимальное количество блоков, которое можно запустить
+
+    //std::cout << "Количество SM на устройстве: " << smCount << std::endl;
+    //std::cout << "Количество блоков, которые можно запустить на SM: " << maxBlocksPerSM << std::endl;
+    //std::cout << "Максимальное количество блоков, которое можно запустить: " << maxBlocks << std::endl;
+
+    //cudaFuncAttributes kernelAttributes;
+    //cudaFuncGetAttributes(&kernelAttributes, applyBlurTexture);
+    //size_t sharedMemoryBytes = kernelAttributes.sharedSizeBytes;
+
+    //std::cout << "Разделяемая память, требуемая ядром: " <<  sharedMemoryBytes << " байт" << std::endl;;
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    if (type == "global") {
+        applyBlurGlobal<<<GS,BS>>>(d_inputImage, d_outputImage, width, height);
+    } else if (type == "shared") {
+        applyBlurShared<<<GS,BS_s>>>(d_inputImage, d_outputImage, width, height);
+    } else if (type == "texture") {
+        applyBlurTexture<<<GS, BS>>>(d_outputImage, width, height);
+    }
+
+    cudaDeviceSynchronize();
+
+    auto end_time = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time).count();
+
+    std::cout << "Time (" << type <<  "): " << duration << "us." << std::endl;
+
+    if (type == "texture") {
+        cudaUnbindTexture(texRef);
+    }
 
     cudaMemcpy(outputImage, d_outputImage, imageSize, cudaMemcpyDeviceToHost);
 
-    for (int i = 0; i < height; i++) {
-        memcpy(row_pointers[i],
-               &outputImage[i * width * 3 * sizeof(unsigned char)],
-               width * 3 * sizeof(unsigned char));
-    }
-
-    if (!saveImage(outputFileName, &row_pointers, width, height)) return 1;
-
     cudaFree(d_inputImage);
     cudaFree(d_outputImage);
+}
+ 
+int main() {
+    const char *inputFileName = "input1024.png";
+    int width, height;
+    png_bytep *row_pointers;
+
+    if (!loadImage(inputFileName, &row_pointers, width, height)) return 1;
+
+    size_t imageSize = width * height * CHANNELS * sizeof(unsigned char);
+    auto *inputImage = new unsigned char[imageSize];
+    auto *outputImage = new unsigned char[imageSize];
+
+    for (int i = 0; i < height; i++) {
+        memcpy(&inputImage[i * width * CHANNELS * sizeof(unsigned char)],
+               row_pointers[i],
+               width * CHANNELS * sizeof(unsigned char));
+    }
+
+    std::vector<std::string> types = {"global", "shared", "texture"};
+
+    for (const auto& type : types) {
+        applyBlur(inputImage, outputImage, imageSize, width, height, type);
+
+        for (int i = 0; i < height; i++) {
+            memcpy(row_pointers[i],
+               &outputImage[i * width * CHANNELS * sizeof(unsigned char)],
+               width * CHANNELS * sizeof(unsigned char));
+        }
+
+        std::string outputFileName = "output/" + type + ".png";
+        if (!saveImage(outputFileName.c_str(), &row_pointers, width, height)) return 1;
+    }
 
     delete[] inputImage;
     delete[] outputImage;
